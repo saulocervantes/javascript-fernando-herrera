c02702f2c5960756edcc94ccc0eec32fab086e4c
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-19 13:30:36 -0500

    13. Orden y lugar de las importaciones

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,26 @@
+
+
+alert("Hola desde app.js");
+
+// console.log(console.log("Hola Mundo"));
+
+let a = 10,
+    b = 20,
+    c = "Hola ",
+    d = "Spiderman",
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log("%c Mis variables", "color:blue; font-weight: bold");
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+c = "Hola de nuevo"
+
+// console.table({a,b,c,d,x});
+
+var miNombre = "Fernando";
+
+// 13. Orden y lugar de las importaciones

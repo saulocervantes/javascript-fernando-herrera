a478766fcc06161cc74f16c8d0a072afe5330a15
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-08-19 14:18:24 -0500

    15. Prompt, confirm y alert

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,1 +1,14 @@
 
+alert("Hola Mundo");
+
+
+let nombre = prompt("¿Cuál es tu nombre?");
+console.log(nombre);
+console.log("****" + nombre + "****"); // ""
+
+const seleccion = confirm("¿Estás seguro de borrar esto?");
+console.log(seleccion);
+
+// console.log(global);
+
+// 15. Prompt, confirm y alert

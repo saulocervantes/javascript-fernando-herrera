e97503da1404c941f9f3104788c4e2d01c9443e3
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-09-07 15:50:56 -0500

    22. Arreglos

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,33 @@
+
+
+
+// const arr = new Array(10);
+// let arr = [];
+// console.log(arr);
+
+let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log({videoJuegos});
+
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    "Fernado",
+    1+2,
+    function(){},
+    ()=>{},
+    { a:1 },
+    ["X","Megaman","Zero","Dr. Light", [
+        "Dr. Willy",
+        "Woodman"
+    ]]
+];
+
+// console.log({arregloCosas});
+// console.log(arregloCosas[2]);
+// console.log(arregloCosas[7][3]);
+console.log(arregloCosas[7][4][1]);
+
+// 22. Arreglos
+

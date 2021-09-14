253fc5e85d2fc59edadc59001a5c893a89e00f0b
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-09-13 21:44:27 -0500

    36. Pro tip: Asignaciones con operadores

--- 01-fundamentos/assets/js/logica-boolena.js
+++ 01-fundamentos/assets/js/logica-boolena.js
@@ -42,4 +42,23 @@ console.log("4 condiciones", true || true || true || false); // true
 
 console.warn("Asignaciones");
 
-// 35. Lógica booleana
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && "Hola Mundo" && 150;
+const a2 = "Hola" && "Mundo" && soyFalso && true;
+const a3 = soyFalso || "Ya no soy falso";
+const a4 = soyFalso || soyUndefined || soyNull || "Ya no soy falso de nuevo" || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || "Ya no soy falso de nuevo" || true;
+
+console.log({a1, a2, a3, a4, a5});
+
+if ( regresaFalse() && regresaTrue() && (true || false || true)) {
+    console.log("Hacer algo");
+}else{
+    console.log("Hacer otra cosa");
+}
+
+
+// 36. Pro tip: Asignaciones con operadores

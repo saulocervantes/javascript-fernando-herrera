1e496c00a0dea6e14e29e37c6932b163a586b569
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-09-21 15:37:04 -0500

    49. Crear baraja de cartas

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,8 +1,38 @@
+/**
+ * 2C = Two of Clubs
+ * 2D = Two of Diaminds
+ * 2H = Two of Hearts
+ * 2S = Two of Spades
+ */
+
+let deck = [];
+const tipos = ["C","D","H","S"];
+const especiales = ["A","J","Q","K"];
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (const tipo of tipos) {
+            deck.push(`${i}${tipo}`);
+        }
+    }
+
+    for(let tipo of tipos){
+        for (const especial of especiales) {
+            deck.push(`${especial}${tipo}`);
+        }
+    }
+
+    console.log(deck);
+    
+    deck = _.shuffle(deck);
+
+    console.log(deck);
+    return deck;
+};
+
+
+crearDeck();
 
 
 
-
-
-
-
-// 48. Estilo y estructura de nuestro juego
+// 49. Crear baraja de cartas

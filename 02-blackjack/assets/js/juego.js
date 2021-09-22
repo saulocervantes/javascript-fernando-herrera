dd683e8eabd8b61e116f97ae2a14acf6afb982b6
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-09-21 22:04:30 -0500

    51. Valor de cada carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -46,7 +46,17 @@ const pedirCarta = () => {
     return carta;
 };
 
-pedirCarta();
+// pedirCarta();
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length-1);
+    // let puntos = 0;
+    
+    return (isNaN(valor)) ? 
+          ( valor === "A" ) ? 11 : 10
+          : valor * 1;
+};
 
+const valor = valorCarta(pedirCarta());
+console.log({valor});
 
-// 50. Pedir carta
+// 51. Valor de cada carta

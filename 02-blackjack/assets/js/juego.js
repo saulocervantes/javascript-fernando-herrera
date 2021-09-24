f9f8ad4e8ac2e80fbc42b5c696ead4652135fbc1
saulo.cervantes@hotmail.com <saulo.cervantes@hotmail.com>
2021-09-23 21:41:57 -0500

    56. Turno de la computadora

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -5,84 +5,116 @@
  * 2S = Two of Spades
  */
 
-let deck = [];
-const tipos = ["C","D","H","S"];
-const especiales = ["A","J","Q","K"];
+ let deck = [];
+ const tipos = ["C","D","H","S"];
+ const especiales = ["A","J","Q","K"];
+ 
+ let puntosJugador = 0,
+     puntosComputadora = 0;
+ 
+ // Referencias del HTML
+ const btnPedir = document.querySelector("#btnPedir");
+ const btnDetener = document.querySelector("#btnDetener");
+ const divCartasJugador = document.querySelector("#jugador-cartas");
+ const divCartasComputadora = document.querySelector("#computadora-cartas");
+ const puntosHTML = document.querySelectorAll("small");
+ 
+ // Esta función crea un nuevo deck
+ const crearDeck = () => {
+     for (let i = 2; i <= 10; i++) {
+         for (const tipo of tipos) {
+             deck.push(`${i}${tipo}`);
+         }
+     }
+ 
+     for(let tipo of tipos){
+         for (const especial of especiales) {
+             deck.push(`${especial}${tipo}`);
+         }
+     }
+ 
+     // console.log(deck);
+     
+     deck = _.shuffle(deck);
+ 
+     console.log(deck);
+     return deck;
+ };
+ 
+ 
+ crearDeck();
+ 
+ // Esta función me permite tomar una carta
+ const pedirCarta = () => {
+     if(deck.length === 0){
+         throw "No hay cartas en el deck";
+     }
+ 
+     const carta = deck.pop();
+     return carta;
+ };
+ 
+ // pedirCarta();
+ const valorCarta = (carta) => {
+     const valor = carta.substring(0, carta.length-1);
+     // let puntos = 0;
+     
+     return (isNaN(valor)) ? 
+           ( valor === "A" ) ? 11 : 10
+           : valor * 1;
+ };
+ 
+ // turno de la computadora
+ const turnoComputadora = (puntosMinimos) => {
+      do {
+         const carta = pedirCarta();
+         puntosComputadora += valorCarta(carta);
+         puntosHTML[1].innerText = puntosComputadora;
+ 
+         // <img class="carta" src="assets/cartas/3C.png" alt="">
+         const imgCarta = document.createElement("img");
+         imgCarta.src = `assets/cartas/${carta}.png`;
+         imgCarta.classList.add("carta");
+         divCartasComputadora.append(imgCarta);
+ 
+         if (puntosMinimos > 21) {
+             break;
+         }
+         
+      } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+ };
+ 
+ // Eventos
+ btnPedir.addEventListener("click", ()=>{
+     const carta = pedirCarta();
+     puntosJugador += valorCarta(carta);
+     puntosHTML[0].innerText = puntosJugador;
+     
+     // <img class="carta" src="assets/cartas/2C.png" alt="">
+     const imgCarta = document.createElement("img");
+     imgCarta.src = `assets/cartas/${carta}.png`;
+     imgCarta.classList.add("carta");
+     divCartasJugador.append(imgCarta);
+ 
+     if (puntosJugador > 21) {
+         console.warn("Lo siento mucho, perdiste");
+         btnPedir.disabled = true;
+         turnoComputadora(puntosJugador);
+         btnDetener.disabled = true;
+     }else if (puntosJugador === 21) {
+         console.warn("21, genial!!");
+         btnPedir.disabled = true;
+         turnoComputadora(puntosJugador);
+         btnDetener.disabled = true;
+     }
+ });
+ 
+ btnDetener.addEventListener("click", ()=>{
+     btnPedir.disabled = true;
+     btnDetener.disabled = true;
+     turnoComputadora(puntosJugador);
+ });
 
-let puntosJugador = 0,
-    puntosComputadora = 0;
-
-// Referencias del HTML
-const btnPedir = document.querySelector("#btnPedir");
-const divCartasJugador = document.querySelector("#jugador-cartas");
-const puntosHTML = document.querySelectorAll("small");
-
-// Esta función crea un nuevo deck
-const crearDeck = () => {
-    for (let i = 2; i <= 10; i++) {
-        for (const tipo of tipos) {
-            deck.push(`${i}${tipo}`);
-        }
-    }
-
-    for(let tipo of tipos){
-        for (const especial of especiales) {
-            deck.push(`${especial}${tipo}`);
-        }
-    }
-
-    // console.log(deck);
-    
-    deck = _.shuffle(deck);
-
-    console.log(deck);
-    return deck;
-};
 
 
-crearDeck();
-
-// Esta función me permite tomar una carta
-const pedirCarta = () => {
-    if(deck.length === 0){
-        throw "No hay cartas en el deck";
-    }
-
-    const carta = deck.pop();
-    return carta;
-};
-
-// pedirCarta();
-const valorCarta = (carta) => {
-    const valor = carta.substring(0, carta.length-1);
-    // let puntos = 0;
-    
-    return (isNaN(valor)) ? 
-          ( valor === "A" ) ? 11 : 10
-          : valor * 1;
-};
-
-// Eventos
-btnPedir.addEventListener("click", ()=>{
-    const carta = pedirCarta();
-    puntosJugador += valorCarta(carta);
-    puntosHTML[0].innerText = puntosJugador;
-    
-    // <img class="carta" src="assets/cartas/2C.png" alt="">
-    const imgCarta = document.createElement("img");
-    imgCarta.src = `assets/cartas/${carta}.png`;
-    imgCarta.classList.add("carta");
-    divCartasJugador.append(imgCarta);
-
-    if (puntosJugador > 21) {
-        console.warn("Lo siento mucho, perdiste");
-        btnPedir.disabled = true;
-    }else if (puntosJugador === 21) {
-        console.warn("21, genial!!");
-        btnPedir.disabled = true;
-    }
-});
-
-
-
-// 55. Crear cartas en el HTML
+ // 56. Turno de la computadora
